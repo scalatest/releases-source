807872900f889563d39c3677d5eabea91160cefa
Chua Chee Seng <cheeseng@amaseng.com>
2026-03-28 17:51:59 +0800

    Added release source for scalatest+scalacheck 3.2.20.0.

---
+++ scalatestplus-scalacheck/3.2.20.0/src/main/scala/org/scalatestplus/scalacheck/FailureMessages.scala
@@ -0,0 +1,34 @@
+package org.scalatestplus.scalacheck
+
+private[scalacheck] object FailureMessages {
+
+def decorateToStringValue(prettifier: org.scalactic.Prettifier, o: Any): String = prettifier.apply(o)
+
+def propCheckLabel(): String = Resources.propCheckLabel()
+
+def propCheckLabels(): String = Resources.propCheckLabels()
+
+def propertyException(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.propertyException(prettifier.apply(param0))
+
+def propCheckExhausted(prettifier: org.scalactic.Prettifier, param0: Any, param1: Any): String = Resources.propCheckExhausted(prettifier.apply(param0), prettifier.apply(param1))
+
+def propCheckExhaustedAfterOne(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.propCheckExhaustedAfterOne(prettifier.apply(param0))
+
+def propertyFailed(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.propertyFailed(prettifier.apply(param0))
+
+def propertyCheckSucceeded(): String = Resources.propertyCheckSucceeded()
+
+def thrownExceptionsLocation(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.thrownExceptionsLocation(prettifier.apply(param0))
+
+def occurredOnValues(): String = Resources.occurredOnValues()
+
+def thrownExceptionsMessage(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.thrownExceptionsMessage(prettifier.apply(param0))
+
+def bigProblems(): String = Resources.bigProblems()
+
+def bigProblemsWithMessage(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.bigProblemsWithMessage(prettifier.apply(param0))
+
+def initSeed(prettifier: org.scalactic.Prettifier, param0: Any): String = Resources.initSeed(prettifier.apply(param0))
+
+}
+    
